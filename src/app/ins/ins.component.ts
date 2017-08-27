89af7f81415509c306b36f12a707655adcba471d
Alexis <alexis.phanor@gmail.com>
2017-08-27 21:52:46 +0100

    Added success message to subscription page

--- src/app/ins/ins.component.ts
+++ src/app/ins/ins.component.ts
@@ -12,6 +12,8 @@ const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA
 
 export class InsComponent implements OnInit {
 
+    private submitted:boolean = false;
+
     constructor(private http: HttpServiceService) {}
 
     emailFormControl = new FormControl('', [
@@ -21,6 +23,7 @@ export class InsComponent implements OnInit {
     nSubmit(w,x,y,z) {
         this.http.postData(w,x,y,z).subscribe(data => {
             console.log(data);
+            this.submitted = true;
         },
         error => console.error('An error occured.'));
     }
